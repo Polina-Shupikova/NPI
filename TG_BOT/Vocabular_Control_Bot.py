e3ce102539963936f81d268730d61bc1d64cb7f3
Ukinnne <osolovegor@gmail.com>
2025-03-26 11:57:43 +1000

    Add files via upload

--- TG_BOT/Vocabular_Control_Bot.py
+++ TG_BOT/Vocabular_Control_Bot.py
@@ -1,29 +1,32 @@
 import telebot
+from telebot import types
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def handler_start(message):
-    bot.send_message(message.chat.id, 'Приветствую товарищ!')
-    bot.send_message(message.chat.id, 'Этот бот-кроссворд предназначем для контроля лексического словарного запаса среди нашего пролетариата')
+    bot.send_message(message.chat.id, 'Приветствую, товарищ!')
+    bot.send_message(message.chat.id, 'Этот бот-кроссворд предназначен для контроля лексического словарного запаса среди нашего пролетариата.')
+    
     markup = types.InlineKeyboardMarkup(row_width=2)
-    btn_yes = types.InlineKeyboardButton("Да", callback_data="yes")
+    web_app_url = "https://example.com"
+    btn_yes = types.InlineKeyboardButton(
+        text="Да",
+        web_app=types.WebAppInfo(url=web_app_url)
+    )
     btn_no = types.InlineKeyboardButton("Нет", callback_data="no")
-
+    
+    markup.add(btn_yes, btn_no)
     bot.send_message(message.chat.id, "Начать?", reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
-    if call.data == "yes":
-        bot.send_message(call.message.chat.id, "Вы выбрали Да! ✅")
-    elif call.data == "no":
-        bot.send_message(call.message.chat.id, "Вы выбрали Нет! ❌")
+    if call.data == "no":
+        bot.send_message(call.message.chat.id, "Плаке плаке")
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
